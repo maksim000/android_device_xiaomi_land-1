629ed540d45b00edccfbc1c65fff35a627370e0a
andrej12333 <farina.andrej@gmail.com>
2018-08-04 16:17:58 +0000

    Prepared for Liquid Remix

--- liquid.mk
+++ liquid.mk
@@ -20,8 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common AEX stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common Liquid Remix stuff
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +30,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 3S
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_land
+PRODUCT_NAME := liquid_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
